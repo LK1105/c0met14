5b97e2b322eecbc078b2780d8f28466c2eca59be
lb <lb@gmail.com>
2021-02-15 09:10:49 +0300

    sandbox

---
+++ c0metjb_rc1/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  c0metjb_rc1
+//
+//  Created by Ali on 15.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ c0metjb_rc1/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  c0metjb_rc1
+//
+//  Created by Ali on 15.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
